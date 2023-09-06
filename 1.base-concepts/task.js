56df0626ef6b9f2683e7748efbe69281baa4d459
Polina Vasilkova <illilis@mail.ru>
2023-09-06 15:31:26 +0300

    Первое задание готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,9 +2,29 @@
 function solveEquation(a, b, c) {
   let arr = [];
   
+  let discriminant = (b ** 2) - (4 * a * c);
+
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr.push((-b + Math.sqrt(discriminant) ) / (2 * a), (-b - Math.sqrt(discriminant) ) / (2 * a));
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  if (isNaN(percent) && isNaN(contribution) && isNaN(amount)) {
+    return false;
+  }
+
+  let percentMonth = percent / 100 / 12;
+  let bodyCredit = amount - contribution;
   
+  let monthlyPayment = bodyCredit * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) - 1)));
+  let totalSum = parseFloat((monthlyPayment * countMonths).toFixed(2));
+  
+  return totalSum;
 }
