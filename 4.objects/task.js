e871f61acd39298ae15c7e7888ca67e0f7b91dd5
Polina Vasilkova <illilis@mail.ru>
2023-09-13 10:32:53 +0300

    Генерация новых студентов и значений

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -29,3 +29,21 @@ Student.prototype.exclude = function (reason) {
 
   this.excluded = reason;
 }
+
+let student1 = new Student("Полина", "женский", 21);
+let student2 = new Student("Игорь", "мужской", 22);
+let student3 = new Student("Дарья", "женский", 20);
+
+student1.setSubject("Философия");
+student2.setSubject("Информатика");
+student3.setSubject("Право");
+
+student1.addMarks(4, 5, 5, 5);
+student2.addMarks(5, 4);
+student3.addMarks(3, 4, 5, 4, 4);
+
+console.log(student1.getAverage());
+console.log(student2.getAverage());
+console.log(student3.getAverage());
+
+student2.exclude("пропуск занятий");
